4a1fc6102fdf60b2443809bc0279f3287ddc2943
appjacker <pushkal.sinha@prontoitlabs.com>
2015-12-01 13:21:06 +0530

    removing items from dom in progress

--- src/app/components/AppRoot.js
+++ src/app/components/AppRoot.js
@@ -11,11 +11,6 @@ class AppRoot extends React.Component {
 
   /*
    * AppRootly PureRenderMixin
-   *
-   * in React 0.13 there is no way to attach mixins to ES6 classes
-   * this is a workaround to solve this
-   * http://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#mixins
-   *
    * @method shouldComponentUpdate
    * @returns {Boolean}
    */
--- src/app/components/Cart.js
+++ src/app/components/Cart.js
@@ -5,29 +5,39 @@ import Item from './Item';
  * @class Cart
  * @extends React.Component
  */
-class Cart extends React.Component {
+ class Cart extends React.Component {
+
+
 
   /*
    * @method shouldComponentUpdate
    * @returns {Boolean}
    */
-  shouldComponentUpdate () {
+   shouldComponentUpdate () {
     return React.addons.PureRenderMixin.shouldComponentUpdate.apply(this, arguments);
   }
 
   
    // * @method render
    // * @returns {JSX}
-   
-  render () {
+
+
+   render () {
     return <div className="cart">
-      <h2>{this.props.cart.title}</h2>
-      <ul>
-        {this.props.cart.items.map(function (item, key) {
-          return <Item key={key} item={item} />;
-        })}
-      </ul>
+    <button className="pull-right"type="button" onClick={this.clearCart}>Clear Cart</button>
+    <h2>{this.props.cart.title}</h2>
+    <ul>
+    {this.props.cart.items.map(function (item, key) {
+      return <Item key={key} item={item} />;
+    })}
+    </ul>
     </div>;
+  }
+
+  clearCart () {
+    // render : function(){
+      return ('', document.getElementsByClassName("cart"));
+    // }
   }
 }
 
